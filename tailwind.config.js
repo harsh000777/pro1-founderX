3ce64d19ec972c0d383d1ce7991a5ceb564e20d0
harsh000777 <harshgoyal7705@gmail.com>
2024-04-07 00:14:56 +0530

    founderx
    hlo

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#0C2225',
+        "nav": '#0A1B1E',
+        "bord": '#CEC0B0',
+        "btn-color": '#CEC0B0',
+        "high": '#cec0b0',
+        "bg-other": '#153c41',
+        "other": '#13363A',
+        "people": '#D1C0B1',
+        "people2": '#4F6A6D',
+        
+      },
+    },
+    
+  },
+  plugins: [],
+}
